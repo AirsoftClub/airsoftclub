611c543f936bd8195d5cc2c609e3789df893d423
Nicolas Kasevich <n.kasevich@gmail.com>
2023-09-15 21:22:11 +0200

    fix(static) static folder not pushing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { login } from "@/services/authService";
@@ -6,7 +6,9 @@ import { login } from "@/services/authService";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => login("n.kasevich@gmail.com", "1234")}>Click me</Button>
+      <Button onClick={() => login("n.kasevich@gmail.com", "1234")}>
+        Click me
+      </Button>
     </main>
-  )
+  );
 }
